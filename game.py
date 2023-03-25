945e67b453631f3349b30b1aa5342a35a22aa3f0
palakknows <20215114.palak@gmail.com>
2023-03-25 12:27:27 +0530

    fourth commit

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Directions(Enum):
     DOWN =4
 
 BOX_SIZE=20
-
+SPEED = 50 #decides how fast our game is
 start_point= namedtuple('start_point', 'x,y')  
     
 class pythongame:
@@ -30,12 +30,12 @@ class pythongame:
         pygame.display.set_caption('SNAKE UHOOO!')#adds caption on screen
         font = pygame.font.Font('font.ttf',32)
         self.clock = pygame.time.Clock()
-        self.screen.fill((58,58,58))
+        
 
         #initial state of game
         self.direction = Directions.RIGHT
 
-        self.head = start_point(self.w/2, self.h/2)
+        self.head = start_point(self.width/2, self.height/2)
 
         self.creeper = [self.head,start_point(self.head.x-BOX_SIZE, self.head.y),
         start_point(self.head.x-(2*BOX_SIZE), self.head.y)]
@@ -47,8 +47,8 @@ class pythongame:
 
     def _place_reward(self):
         #to exclude boundaries from either sides
-        x= random.randint((0, (self.width-(2*BOX_SIZE)))//BOX_SIZE)*BOX_SIZE
-        y= random.randint((0, (self.height-(2*BOX_SIZE)))//BOX_SIZE)*BOX_SIZE
+        x= random.randint(0, (self.width-2*BOX_SIZE)//BOX_SIZE)*BOX_SIZE #//division operator
+        y= random.randint(0, (self.height-2*BOX_SIZE)//BOX_SIZE)*BOX_SIZE
         self.reward = start_point(x,y)
         #to avoid placiing food on snake
         if self.reward in self.creeper:
@@ -59,21 +59,42 @@ class pythongame:
     def play_game(self):
         #action from outside that is input is taken
 
+        #move 
+
+        #exit 
+
+        #location upgrade for food
+
+        #update of display
+        self._update_display()
+        self.clock.tick(SPEED)
+
+        #score is seen and status of game
+        game_on = True
+        return game_on,self.score
+    
+    def _update_display(self):
+        self.screen.fill((58,58,58)) #color of screen grey
 
 
 
 
-     game_is_on = True
-     return self.score
-
-
-
+#defining main function 
+if __name__ == "__main__":
+    pyg=pythongame()
+    
 #the loop for game when it has to exit 
-while game_is_on:
+while True:
+
+    game_on,score = pyg.play_game()
+
+    if game_on ==False:
+        break
     for event in pygame.event.get(): #all events covered
         if event.type == pygame.QUIT: #for quit button use
-            game_is_on  = False
-        
+            game_on  = False
+    print('Final score',score) #mentioned as in initialising func
+    
 #color remains for complete game so adding it here
         
 
