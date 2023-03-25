adf3afe9784a1d59289b779a71c05cbb2f2b7b10
palakknows <20215114.palak@gmail.com>
2023-03-25 22:53:12 +0530

    commit

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import pygame
+import os
 import random
+import numpy as np
+import pygame.freetype
 from enum import Enum
+from sys import exit
+from pygame.locals import *
 from collections import namedtuple
 
 
 # initialising the pygame 
-pygame .init()
+pygame.init()
+pygame.font.init()
 
-font = pygame.font.Font('font.ttf',30)
+font = pygame.freetype.SysFont('font.ttf',30)
 #directions of action 
 class Directions(Enum):
     RIGHT = 1
@@ -15,11 +21,13 @@ class Directions(Enum):
     UP =3
     DOWN =4
 
+    BLACK=(0,0,0)
+
 BOX_SIZE=20
-SPEED = 10 #decides how fast our game is
+SPEED = 20 #decides how fast our game is
 start_point= namedtuple('start_point', 'x,y')  
     
-class pythongame:
+class pythongameAI:
     def __init__(self, width=684 ,height=480):
         self.width = width
         self.height = height
@@ -32,8 +40,12 @@ class pythongame:
        
         self.clock = pygame.time.Clock()
         
+        self.refresh()
+       
 
-        #initial state of game
+
+    def refresh(self):
+         #initial state of game
         self.direction = Directions.RIGHT
 
         self.head = start_point(self.width/2, self.height/2)
@@ -43,6 +55,7 @@ class pythongame:
         self.score = 0
         self.reward =None
         self._place_reward()
+        self.frame_iteration=0
 
 
     def _place_reward(self):
@@ -57,40 +70,37 @@ class pythongame:
 
 
 
-    def play_game(self):
+    def play_game(self, action):
+        self.frame_iteration+=1
         #action from outside that is input is taken
+    
         for event in pygame.event.get(): 
           if event.type == pygame.QUIT:
             pygame.quit()#ensures game is not running
             quit()                              
             
-          if event.type == pygame.KEYDOWN:
-            
-            if event.key == pygame.K_LEFT:
-                self.direction =Directions.LEFT
-            elif event.key == pygame.K_RIGHT:
-                self.direction =Directions.RIGHT
-            elif event.key == pygame.K_UP:
-                self.direction =Directions.UP
-            elif event.key == pygame.K_DOWN:
-                self.direction =Directions.DOWN
-        #move 
-        self._move(self.direction)#move is defined
+    
+        #movement guide
+        self._move(action)#move is defined
         self.creeper.insert(0,self.head)#adding head 
 
     
 
         #exit 
-        game_on=True
-        if self._collision():
-            game_on=False
-            return game_on,self.score
+        prize =0
+        game_over=False
+        if self._collision() or self.frame_iteration> 120*len(self.creeper):
+            game_over=True
+            prize=-10
+
+            return prize,game_over,self.score
         
 
         #location upgrade for food
 
         if self.head==self.reward:
-            self.score+=1
+            self.score +=1
+            prize= 10
             self._place_reward()
         else:
             self.creeper.pop()#remove food from end
@@ -100,61 +110,60 @@ class pythongame:
         self.clock.tick(SPEED)
 
         #score is seen and status of game
-        return game_on,self.score
+        return prize,game_over,self.score
     
     
-    def _collision(self):
-        if self.head.x>self.width-BOX_SIZE or self.head.y>self.height-BOX_SIZE or self.head.x<0 or self.head.y<0 :
-         return False
+    def _collision(self,pt=None):
+        if pt is None:
+            pt = self.head
+        if pt.x>self.width - BOX_SIZE or pt.y>self.height-BOX_SIZE or pt.x<0 or pt.y<0 :
+         return True
         
-        if self.head in self.creeper[1:]:
-         return False
+        if pt in self.creeper[1:]:
+         return True
         
-        return True
+        return False
 
     
     def _update_display(self):
         self.screen.fill((58,58,58)) #color of screen grey
         
-        for start_point in self.creeper:
-            pygame.draw.rect(self.screen,(0,255,0),pygame.Rect(start_point.x,start_point.y,BOX_SIZE,BOX_SIZE))#ensures snake on screen of green color and rectangles
-            pygame.draw.rect(self.screen,(255,0,0),pygame.Rect(start_point.x+6,start_point.y+6,10,10))#a small rect in front of red color
+        for pt in self.creeper:
+            pygame.draw.rect(self.screen,(0,255,0),pygame.Rect(pt.x,pt.y,BOX_SIZE,BOX_SIZE))#ensures snake on screen of green color and rectangles
+            pygame.draw.rect(self.screen,(255,0,0),pygame.Rect(pt.x+6,pt.y+6,10,10))#a small rect in front of red color
 
             pygame.draw.rect(self.screen,(0,0,255),pygame.Rect(self.reward.x,self.reward.y,10,10))
         #concatenation of string 
-            text=font.render("Score:"+str(self.score), True,(0,0,0))
-            self.screen.blit(text, [0,0])#to display at corner
+            text=font.render('Score:' + str(self.score), True,(0,0,0))
+            self.screen.blit(text,[0,0])
             pygame.display.flip()#section of screen is refreshed
-    def _move(self,direction):
+    def _move(self,action):
+         
+        motion=[Directions.RIGHT, Directions.DOWN,Directions.LEFT,Directions.UP]
+        idx= motion.index(self.direction)
+        
+        if np.array_equal(action,[1,0,0]):
+            new_dir = motion[idx]
+        elif np.array_equal(action,[0,1,0]):
+            next_idx =(idx+1)%4
+            new_dir = motion[next_idx]
+        else:
+            next_idx =(idx-1)%4
+            next_dir=motion[next_idx]
+        
+        self.direction =new_dir
+
         x=self.head.x
         y=self.head.y
-        if direction == Directions.RIGHT:
+        if self.direction == Directions.RIGHT:
             x+=BOX_SIZE #moving one box ahead
-        elif direction == Directions.LEFT:
+        elif self.direction == Directions.LEFT:
             x-=BOX_SIZE
-        elif direction == Directions.UP:
+        elif self.direction == Directions.UP:
             y+=BOX_SIZE
-        elif direction == Directions.DOWN:
+        elif self.direction == Directions.DOWN:
             y-=BOX_SIZE
 
         self.head = start_point(x,y)
 
 #defining main function 
-if __name__ == "__main__":
-    pyg=pythongame()
-    
-#the loop for game when it has to exit 
-while True:
-
-    game_on,score = pyg.play_game()
-
-    if game_on ==False:
-        break
-    
-    print('Final score',score) #mentioned as in initialising func
-    
-#color remains for complete game so adding it here
-        
-    pygame.quit()
-        
-    
